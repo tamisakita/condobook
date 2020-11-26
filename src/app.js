9813588fda329f6f340675f7a64e13fbb59e2aac
isisabreu <isisrcabreu@gmail.com>
2020-11-26 20:15:29 -0300

    filling app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,27 @@
-let Isis = "hello world";
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+import MongoConnection from './configs/db.config';
+import apiRoutes from './routes/api.routes';
+
+dotenv.config();
+
+class App {
+    constructor() {
+        this.app = express();
+        this.mongoConnection = new MongoConnection(process.env.MONGODB_URI);
+
+        this.connectToDatabase();
+        this.activateAppRoutes();
+    }
+    connectToDatabase() {
+        this.mongoConnection.startDbConnection();
+    }
+    activateAppRoutes() {
+        this.app.use('/api', apiRoutes);
+      }
+}
+
+export default new App().app;
