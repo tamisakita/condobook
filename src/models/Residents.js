6e6031f82e6bc44b21465dd5711b62eaf656dcc1
Tami Pipoca <tamisakita@hotmail.com>
2020-11-28 17:45:23 -0300

    improved residents schema

--- src/models/Residents.js
+++ src/models/Residents.js
@@ -1,4 +1,5 @@
 import { Schema, model } from 'mongoose';
+import joi from 'joi';
 
 const residentSchema = new Schema(
   {
@@ -19,6 +20,12 @@ const residentModel = model('Residents', residentSchema);
 class ResidentEntity {
   constructor () {
     this.Residents = residentModel;
+
+    this.fullName = joi.string().min(5).max(100).required();
+    this.password = joi.string().min(5).max(100).required();
+    this.email = joi.string().email().min(5).max(10).required();
+    this.apartment = joi.string().min(5).max(100).required();
+    this.phone = joi.string().min(5).max(100);
   }
 
   //metodos que representam regras de negocio referentes exclusivamente ao residents
